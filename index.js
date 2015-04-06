6a221998799f655c527b8f4a02315f3ddd7a5b32
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-04-06 16:25:26 -0700

    Added get request for users

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var express = require('express');
 var where = require('lodash/collection/where');
+var find = require('lodash/collection/find');
 var fixtures = require('./fixtures');
 
 var app = express();
@@ -9,7 +10,8 @@ var server = app.listen(3000, '127.0.0.1');
 
 app.get('/', function (req, res) {
   res.status(200);
-  res.end('Hello!');
+  res.set('Content-Type', 'text/plain');
+  res.send('Hello!');
 });
 
 app.get('/api/tweets', function (req, res) {
@@ -24,16 +26,15 @@ app.get('/api/tweets', function (req, res) {
   }
 });
 
-app.get('/api/users', function (req, res) {
-  if (req.query.id) {
-    res.status(200);
-    res.set('Content-Type', 'application/json');
-    var tweets = where(fixtures.users, {userId: req.query.id});
-    tweets.sort(compareByTime);
-    res.json({'tweets': tweets});
-  } else {
-    res.status(400).send('Bad Request');
-  }
+app.get('/api/users/:userId', function (req, res) {
+    var user = find(fixtures.users, {id: req.params.userId});
+    if (user) {
+      res.status(200);
+      res.set('Content-Type', 'application/json');
+      res.json({'user': user});
+    } else {
+      res.status(404).send('Not Found');
+    }
 });
 
 /* Comparison function for Tweets. Tweets created more recently are
@@ -47,6 +48,6 @@ function compareByTime(a, b) {
     } else {
       return 0;
     }
-  }
+  };
 
 module.exports = server;
