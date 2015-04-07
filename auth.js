ff859011e94de443a5a4f1b9d30c478bc13f1e73
Jeffrey Chan <jeffreychan637@berkeley.edu>
2015-04-06 17:51:38 -0700

    Implemented serialize and de-serialize methods for passport

--- auth.js
+++ auth.js
@@ -1,3 +1,20 @@
 'use strict';
 
-var passport = require('passport');
+var passport = require('passport'),
+    _find = require('lodash/collection/find'),
+    fixtures = require('./fixtures');
+
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  var user = _find(fixtures.users, {id: id});
+  if (user) {
+    done(null, user);
+  } else {
+    done(null, false);
+  }
+});
+
+module.exports = passport;
